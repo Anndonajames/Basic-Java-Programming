cf3deff17ce6f5b5257ae912dbd5c53b5b8eea21
FACE-7 <FACE-7@FACE-7-PC>
2019-10-22 18:34:06 +0530

    Basic Programing

---
+++ basicprogram/src/program/leapyear.java
@@ -0,0 +1,50 @@
+package program;
+import java.util.Scanner;
+public class leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter a year");
+	int year=s.nextInt();
+	/*if(year%4 == 0)
+	{
+		
+		if(year%100==0)
+		{
+			if(year%400==0)
+			{
+				System.out.println("leapyear");
+			}
+			else
+			{
+				System.out.println("Not leapyear");
+			}
+		}
+		else
+		{
+		System.out.println("leapyear");
+		}
+	}
+	else
+	{
+	System.out.println("Not leapyear");
+	}
+	
+		
+		*/
+	if(year %4 ==0 && year%100!=0 || year%400==0)
+
+		{
+		System.out.println("leapyear");
+		}
+		
+	else
+	{
+		System.out.println("Not leapyear");
+	}
+	s.close();
+	}
+	
+
+}
